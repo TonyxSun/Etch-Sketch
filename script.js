c46b1ca8cb20776c5d3cbeed2429ac22188050e1
Tony Sun <sunwuyue@live.com>
2021-09-05 22:48:45 -0700

    Final Design

--- script.js
+++ script.js
@@ -1,13 +1,60 @@
+let COLOR = "#000000";
+let MODE = "default";
 const container = document.querySelector('.container');
-for (let i = 0; i < 16; i++) {
-    let div = document.createElement('div');
-    div.setAttribute('id', `${i}`);
-    div.classList.add('grid-item');
-    container.appendChild(div);
 
+function createGrid(size){
+    container.setAttribute('style', `grid: repeat(${size}, 1fr) / repeat(${size}, 1fr);`);
+    if (MODE === "default"){
+        container.setAttribute('style', `box-shadow: 0 0 10px ${COLOR};`);
+    } else {
+        container.setAttribute('style', `box-shadow: 0 0 10px #FFFFFF;`);
+    }
+    for (let i = 0; i < size**2; i++) {
+        let div = document.createElement('div');
+        div.setAttribute('id', `${i}`);
+        div.classList.add('grid-item');
+        // console.log(COLOR);
+        div.addEventListener('mouseenter', changeColor);
+        container.appendChild(div);
+    } 
 }
 
+function changeColor(e){
+    let tempColor;
+    if (MODE == "rainbow") {
+        tempColor = `hsl(${getRandomNumber(360)}deg,${getRandomNumber(100)}%,${getRandomNumber(100)}%)`;
+        e.target.style[`background-color`] = tempColor;
+        return;
+    } 
+    // console.log(e);
+    e.target.style[`background-color`] = COLOR;
+}
 
-divs = document.querySelectorAll('.grid-item');
-divs.forEach(div => div.addEventListener('mouseenter', e => div.classList.add('hovered')));
-// clearBtn = document.qu
+const getRandomNumber = (maxNum) => {
+    return Math.floor(Math.random() * maxNum);
+  };
+
+function resetAll(){
+    container.innerHTML = "";
+    createGrid(rangeslider.value);
+
+}
+const divs = document.querySelectorAll('.grid-item');
+const clearBtn = document.querySelector('#clearBtn');
+const rangeslider = document.getElementById("sliderRange");
+const sliderOutput = document.getElementById("sliderVal");
+const rainbowBtn = document.querySelector('#rainbowBtn');
+const colorBtn = document.querySelector('#colorBtn');
+const colorPicker = document.getElementById("colorPicker");
+
+colorPicker.onchange = (e) => {COLOR = e.target.value; container.style.boxShadow = `0 0 8px ${COLOR}`};
+clearBtn.addEventListener('click',resetAll);
+colorBtn.addEventListener('click', () => {MODE = "default"; resetAll();});
+rainbowBtn.addEventListener('click', () =>{MODE = "rainbow";resetAll();});
+sliderOutput.innerHTML = `${rangeslider.value} x ${rangeslider.value}`;
+rangeslider.oninput = function() {
+    sliderOutput.innerHTML = `${this.value} x ${this.value}`;
+    resetAll();
+}
+
+createGrid(rangeslider.value);
